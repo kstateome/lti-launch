34f22f910a93f9f34d36a583f13f82af3619c2da
Killian Stone <killsto@ksu.edu>
2016-06-17 09:51:42 -0500

    Statically importing Assert

--- src/test/java/edu/ksu/lti/launch/test/SpringContextITest.java
+++ src/test/java/edu/ksu/lti/launch/test/SpringContextITest.java
@@ -2,18 +2,19 @@ package edu.ksu.lti.launch.test;
 
 
 import edu.ksu.lti.launch.controller.OauthController;
-import edu.ksu.lti.launch.oauth.LtiLaunchCanvas;
+import edu.ksu.lti.launch.oauth.LtiLaunch;
 import edu.ksu.lti.launch.spring.config.TestApplicationConfig;
 import edu.ksu.lti.launch.spring.config.TestSpringConfig;
 import edu.ksu.lti.launch.spring.config.TestServiceConfig;
 import edu.ksu.lti.launch.controller.TestLtiLaunchController;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.assertNotNull;
 
 /*
  * This test simply tests that the spring context is able to be setup in an appropiate way.
@@ -29,14 +30,14 @@ public class SpringContextITest {
     @Autowired
     private OauthController oauthController;
     @Autowired
-    private LtiLaunchCanvas ltiLaunchCanvas;
+    private LtiLaunch ltiLaunch;
 
     @Test
     public void testSpringContext() {
         //The test will fail if the spring context is not setup appropriately.
-        Assert.assertNotNull("Expected testLtiLaunchController to be instantiated by Spring", testLtiLaunchController);
-        Assert.assertNotNull("Expected oauthController to be instantiated by Spring", oauthController);
-        Assert.assertNotNull("Expected ltiLaunchCanvas to be instantiated by Spring", ltiLaunchCanvas);
+        assertNotNull("Expected testLtiLaunchController to be instantiated by Spring", testLtiLaunchController);
+        assertNotNull("Expected oauthController to be instantiated by Spring", oauthController);
+        assertNotNull("Expected ltiLaunchCanvas to be instantiated by Spring", ltiLaunch);
     }
 
 }
