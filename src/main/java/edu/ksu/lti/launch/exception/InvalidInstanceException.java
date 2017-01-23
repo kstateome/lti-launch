756e7266b2684bae5452f67bc2aa678e7e891ea5
Toby Murray <toby@ksu.edu>
2017-01-23 15:49:43 -0600

    Adding javadoc to InvalidInstanceException
    Also added a serialVersionUID.

--- src/main/java/edu/ksu/lti/launch/exception/InvalidInstanceException.java
+++ src/main/java/edu/ksu/lti/launch/exception/InvalidInstanceException.java
@@ -1,6 +1,15 @@
 package edu.ksu.lti.launch.exception;
 
+/**
+ * Thrown to indicate that this LTI application is running in an unexpected instance of Canvas.
+ *
+ * This happens most often after a Canvas test/beta overwrite from production. The
+ * production settings are copied to the development instance and then you have a user
+ * in Canvas test trying to hit the production LTI application.
+ */
 public class InvalidInstanceException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
     public final String launchUrl;
     public final String courseId;
 
