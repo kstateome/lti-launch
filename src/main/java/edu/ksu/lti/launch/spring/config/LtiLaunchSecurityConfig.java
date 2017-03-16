1f55662b1ee925ff113742afc7a25d9c21ed64f2
Toby Murray <toby@ksu.edu>
2017-03-16 14:30:50 -0500

    Some more refactoring of OAuth filter setup
    Removed one more class and cleared out some unused cruft from the security config class.

--- src/main/java/edu/ksu/lti/launch/spring/config/LtiLaunchSecurityConfig.java
+++ src/main/java/edu/ksu/lti/launch/spring/config/LtiLaunchSecurityConfig.java
@@ -2,22 +2,18 @@ package edu.ksu.lti.launch.spring.config;
 
 import edu.ksu.lti.launch.oauth.LtiConsumerDetailsService;
 import edu.ksu.lti.launch.oauth.LtiOAuthAuthenticationHandler;
-import edu.ksu.lti.launch.oauth.LTIOAuthProviderProcessingFilter;
 import edu.ksu.lti.launch.service.ConfigService;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.oauth.provider.filter.ProtectedResourceProcessingFilter;
 import org.springframework.security.oauth.provider.nonce.InMemoryNonceServices;
 import org.springframework.security.oauth.provider.token.InMemoryProviderTokenServices;
 import org.springframework.security.oauth.provider.token.OAuthProviderTokenServices;
@@ -27,26 +23,23 @@ import org.springframework.security.web.header.writers.frameoptions.StaticAllowF
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import javax.annotation.PostConstruct;
 import java.net.URI;
 
 /**
- * Common LTI application configuration. A few things that are done here:
- *  - Enable web security on the /launch URL to verify the oauth signature coming from Canvas
- *  - Enable content security policy headers so that browsers are happy
+ * This configuration class sets up Spring Security to authenticate LTI
+ * launch requests based on the OAuth signature present in the POST params.
+ * It also sets up some common HTTP headers that get returned to the browser
+ * on each request to make browsers happy running inside of an iframe.
  */
 @Configuration
 @EnableWebMvcSecurity
-public class LtiLaunchSecurityConfig extends WebMvcConfigurerAdapter implements ApplicationContextAware, ApplicationListener<ContextRefreshedEvent> {
+public class LtiLaunchSecurityConfig extends WebMvcConfigurerAdapter {
 
     private static final Logger LOG = Logger.getLogger(LtiLaunchSecurityConfig.class);
-
-    private static volatile ApplicationContext context;
 
     @Configuration
     @Order(1)
     public static class LTISecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
-        private LTIOAuthProviderProcessingFilter ltioAuthProviderProcessingFilter;
         @Autowired
         private LtiConsumerDetailsService oauthConsumerDetailsService;
         @Autowired
@@ -56,18 +49,6 @@ public class LtiLaunchSecurityConfig extends WebMvcConfigurerAdapter implements 
 
         @Autowired
         private ConfigService configService;
-
-        @PostConstruct
-        public void init() {
-            //Set up nonce service to prevent replay attacks.
-            InMemoryNonceServices nonceService = new InMemoryNonceServices();
-            nonceService.setValidityWindowSeconds(600);
-
-            ltioAuthProviderProcessingFilter = new LTIOAuthProviderProcessingFilter(oauthConsumerDetailsService,
-                                                        nonceService,
-                                                        oauthAuthenticationHandler,
-                                                        oauthProviderTokenServices);
-        }
 
         @Override
         public void configure(WebSecurity web) throws Exception {
@@ -89,7 +70,7 @@ public class LtiLaunchSecurityConfig extends WebMvcConfigurerAdapter implements 
             }
             http.requestMatchers()
                 .antMatchers("/launch").and()
-                .addFilterBefore(ltioAuthProviderProcessingFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(configureProcessingFilter(), UsernamePasswordAuthenticationFilter.class)
                 .authorizeRequests().anyRequest().authenticated().and().csrf().disable()
                 .headers().addHeaderWriter(new XFrameOptionsHeaderWriter(new StaticAllowFromStrategy(new URI(canvasUrl))))
                 .addHeaderWriter(new StaticHeadersWriter("Content-Security-Policy",
@@ -99,21 +80,24 @@ public class LtiLaunchSecurityConfig extends WebMvcConfigurerAdapter implements 
                         "style-src 'self' 'unsafe-inline' https://*.instructure.com https://www.k-state.edu" ))
                 .addHeaderWriter(new StaticHeadersWriter("P3P", "CP=\"This is just to make IE happy with cookies in this iframe\""));
         }
+
+        private ProtectedResourceProcessingFilter configureProcessingFilter() {
+            //Set up nonce service to prevent replay attacks.
+            InMemoryNonceServices nonceService = new InMemoryNonceServices();
+            nonceService.setValidityWindowSeconds(600);
+
+            ProtectedResourceProcessingFilter processingFilter = new ProtectedResourceProcessingFilter();
+            processingFilter.setAuthHandler(oauthAuthenticationHandler);
+            processingFilter.setConsumerDetailsService(oauthConsumerDetailsService);
+            processingFilter.setNonceServices(nonceService);
+            processingFilter.setTokenServices(oauthProviderTokenServices);
+            return processingFilter;
+        }
     }
-    
+
     @Bean(name = "oauthProviderTokenServices")
     public OAuthProviderTokenServices oauthProviderTokenServices() {
         // NOTE: we don't use the OAuthProviderTokenServices for 0-legged but it cannot be null
         return new InMemoryProviderTokenServices();
-    }
-
-    @Override
-    public void setApplicationContext(ApplicationContext context) {
-        this.context = context;
-    }
-
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
-        LOG.info("Context event caught");
     }
 }
