0bcea15f3c26da3763a54da79fd47d5117673ec5
hamdawg <dhammett@ksu.edu>
2020-03-17 14:42:31 -0500

    This should work so long as our projects actually use DefaultCookieSerializer
    for their session

--- src/test/java/edu/ksu/lti/launch/spring/config/TestApplicationConfig.java
+++ src/test/java/edu/ksu/lti/launch/spring/config/TestApplicationConfig.java
@@ -2,13 +2,19 @@ package edu.ksu.lti.launch.spring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 
 @Configuration
 public class TestApplicationConfig {
-    public static final String APPLICATION_NAME = "testLtiLaunch";
+    private static final String APPLICATION_NAME = "testLtiLaunch";
 
     @Bean
     public String applicationName() {
         return APPLICATION_NAME;
+    }
+
+    @Bean
+    public DefaultCookieSerializer fakeDefaultCookieSerializer() {
+        return new DefaultCookieSerializer();
     }
 }
